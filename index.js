93be16ec1e68081a315723048ad6da1239e0fa0d
michaelzoidl <github@michaelzoidl.com>
2018-07-03 12:59:40 +0200

    improve and refactore classnames

--- index.js
+++ index.js
@@ -1,37 +1,27 @@
-export function typeOf(object) {
-  return toString.call(object).slice(8, -1).toLowerCase();
-}
+module.exports = style => {
+  const handleArgs = args => {
+    const concatedStyles = [];
 
-export function handleArguments(style, args) {
-  let concatedStyles = [];
+    args.map(argument => {
+      const constructor = argument && argument.constructor;
 
-  args.forEach(argument => {
-    switch (typeOf(argument)) {
-      case 'string':
-        if (style[argument]) {
-          concatedStyles.push(style[argument]);
-        }
-        break;
-      case 'array':
-        concatedStyles.push(...handleArguments(style, argument))
-        break;
-      case 'object':
-        Object
-          .entries(argument)
-          .forEach(([prop, value]) => {
-            if (!!value && style[prop]) {
-              concatedStyles.push(style[prop]);
-            }
-          })
-        break;
-    }
-  });
+      if (constructor === String && style[argument]) {
+        concatedStyles.push(style[argument]);
 
-  return concatedStyles;
-}
+      } else if (constructor === Array) {
+        concatedStyles.push(...handleArgs(argument));
 
-export default function RnClassnames(style = {}) {
-  return function concator(...args) {
-    return handleArguments(style, args);
-  };
+      } else if (constructor === Object) {
+        Object.entries(argument).map(([prop, value]) => {
+          if (!!value && style[prop]) {
+            concatedStyles.push(style[prop]);
+          }
+        });
+      }
+    });
+
+    return concatedStyles;
+  }
+
+  return (...args) => handleArgs(args);
 }
